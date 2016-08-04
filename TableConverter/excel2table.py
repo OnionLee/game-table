c1db3ad22e38cdc1077eac5dbdda7493292970af
onionlee <rkdgml6332@gmail.com>
2016-08-05 03:54:27 +0900

    리팩토링

--- TableConverter/excel2table.py
+++ TableConverter/excel2table.py
@@ -7,61 +7,82 @@ from collections import OrderedDict
 
 class Table:
 
-    def __init__(self, sheet):
+    def __init__(self, sheet, is_root):
+        self.name = sheet.name
+        self.is_root = is_root
         self.col_names = []
         self.descriptors = []
-        self.name = sheet.name
+        self.idx_map = {}
 
-        self.get_col_names(sheet)
-        self.get_descriptors(sheet)
+        self.init_col_names(sheet)
+        self.init_descriptors(sheet)
+        if is_root:
+            self.init_idx_map()
 
-    def get_col_names(self, sheet):
+    def init_col_names(self, sheet):
         row = sheet.row_values(0)
         for value in row:
             self.col_names.append(value)
 
-    def get_descriptors(self, sheet):
+    def init_descriptors(self, sheet):
         for i in range(1, sheet.nrows):
             row = sheet.row_values(i)
             self.descriptors.append(self.get_descriptor(row))
 
     def get_descriptor(self, row):
         descriptor = OrderedDict()
-        for j in range(0, len(row)):
-            key = self.col_names[j]
+        for i in range(0, len(row)):
+            key = self.col_names[i]
             if key[0] == '_':
                 continue
 
-            descriptor[key] = row[j]
+            descriptor[key] = row[i]
 
         return descriptor
 
-    def get_descriptors_by_id(self, id):
-        descriptors = []
-        for descriptor in self.descriptors:
-            if '*parent' not in descriptor:
-                print(descriptor)
-                print(self.name + ' : this table is root table')
-                return descriptors
-            elif descriptor['*parent'] == id:
-                #*parent를 지우기 위해 카피본을 만듦
-                clone = copy.deepcopy(descriptor)
-                del clone['*parent']
-                descriptors.append(clone)
-
-        return descriptors
-
-    def merge(self, table):
-        self.col_names.append(table.name)
+    def init_idx_map(self):
         for descriptor in self.descriptors:
             id = descriptor['id']
-            descriptor[table.name] = table.get_descriptors_by_id(id)
+            self.idx_map[id] = self.descriptors.index(descriptor)
 
-    def to_json(self, pretty):
-        if pretty:
-            return json.dumps(self.descriptors, ensure_ascii=False, indent=4)
+    def merge_child_table(self, child_table):
+        if child_table.is_root:
+            print(self.name + ' table is root table, merge failed')
+            return
+
+        self.add_child_list(child_table)
+
+        for descriptor in child_table.descriptors:
+            if('*parent' not in descriptor):
+                print(self.name + ' table is not child table')
+                continue
+
+            parent_id = descriptor['*parent']
+            parent_idx = self.idx_map[parent_id]
+            parent_descriptor = self.descriptors[parent_idx]
+
+            child_descriptor = copy.deepcopy(descriptor)
+            del child_descriptor['*parent']
+
+            parent_descriptor[child_table.name].append(child_descriptor)
+
+    def add_child_list(self, child_table):
+        self.col_names.append(child_table.name)
+        for descriptor in self.descriptors:
+            descriptor[child_table.name] = []
+
+    def save_to_json(self, pretty_print):
+        if not self.is_root:
+            print(self.name + ' table is child table, save failed')
+            return
+
+        if pretty_print:
+            string = json.dumps(self.descriptors, ensure_ascii=False, indent=4)
         else:
-            return json.dumps(self.descriptors, ensure_ascii=False)
+            string = json.dumps(self.descriptors, ensure_ascii=False)
+
+        with open(self.name + '.json', 'w') as f:
+            f.write(string)
 
 
 class Converter:
@@ -78,7 +99,7 @@ class Converter:
         sheets = workbook.sheets()
 
         root_sheet = sheets[0]
-        root_table = Table(root_sheet)
+        root_table = Table(root_sheet, True)
         print('Convert starting... root table is ' + root_sheet.name)
 
         for i in range(1, len(sheets)):
@@ -86,11 +107,8 @@ class Converter:
             if(child_sheet.name[0] == '_'):
                 continue
 
-            child_table = Table(child_sheet)
-            root_table.merge(child_table)
+            child_table = Table(child_sheet, False)
+            root_table.merge_child_table(child_table)
 
-        json = root_table.to_json(self.pretty_print)
-        with open(root_table.name + '.json', 'w') as f:
-            f.write(json)
-
+        root_table.save_to_json(self.pretty_print)
         print('Done')
