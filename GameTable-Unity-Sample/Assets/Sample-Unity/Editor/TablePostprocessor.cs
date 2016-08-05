00df045d2f9734da18d373ac4e208458980e0431
onionlee <rkdgml6332@gmail.com>
2016-08-06 07:50:04 +0900

    Remove auto convert

--- GameTable-Unity-Sample/Assets/Sample-Unity/Editor/TablePostprocessor.cs
+++ GameTable-Unity-Sample/Assets/Sample-Unity/Editor/TablePostprocessor.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Diagnostics;
 
 public class TablePostprocessor : AssetPostprocessor 
-{
-	//Edit script path
-	private static string scriptPath = "/Sample-Unity/TableSystem/Excel/convert.py";
+{	
 	private static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
 	{
 		foreach (string str in importedAssets)
 		{
-			if (str.Contains (".xlsx"))
+			if (str.EndsWith(".xlsx"))
 				Convert ();
 		}
 	}
 
 	private static void Convert()
 	{
-		Process.Start (Application.dataPath + scriptPath);
-		UnityEngine.Debug.Log ("Convert");
+		Debug.Log ("TODO : Auto Convert");
 	}
 }
