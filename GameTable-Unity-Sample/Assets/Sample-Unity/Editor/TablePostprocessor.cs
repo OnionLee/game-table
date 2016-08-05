1d9491b57bd914cd9927adce03c7a7f83de02fd1
onionlee <rkdgml6332@gmail.com>
2016-08-06 07:08:01 +0900

    Add convert script in unity project

---
+++ GameTable-Unity-Sample/Assets/Sample-Unity/Editor/TablePostprocessor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class TablePostprocessor : AssetPostprocessor 
+{
+	//Edit script path
+	private static string scriptPath = "/Sample-Unity/TableSystem/Excel/convert.py";
+	private static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
+	{
+		foreach (string str in importedAssets)
+		{
+			if (str.Contains (".xlsx"))
+				Convert ();
+		}
+	}
+
+	private static void Convert()
+	{
+		Process.Start (Application.dataPath + scriptPath);
+		UnityEngine.Debug.Log ("Convert");
+	}
+}
---
+++ GameTable-Unity-Sample/Assets/Sample-Unity/TableSystem/Excel/convert.py
@@ -0,0 +1,13 @@
+import os
+from excel2table import Converter
+
+EXPORT_PATH = '../Resources/Table/'
+
+converter = Converter(True, EXPORT_PATH)
+
+for path, dirs, files in os.walk('./'):
+    for file in files:
+        if os.path.splitext(file)[1].lower() == '.xlsx':
+            converter.convert(file)
+
+input()
