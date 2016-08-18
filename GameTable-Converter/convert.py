e4ca55bd1649bf7d97628034a5029fe4c527287b
onionlee <rkdgml6332@gmail.com>
2016-08-19 02:31:25 +0900

    엑셀 임시파일 수정

--- GameTable-Converter/convert.py
+++ GameTable-Converter/convert.py
@@ -7,6 +7,10 @@ converter = Converter(True, EXPORT_PATH)
 
 for path, dirs, files in os.walk('./'):
     for file in files:
+        # ignore temporary excel file
+        if '~$' in str(file):
+            continue
+
         if os.path.splitext(file)[1].lower() == '.xlsx':
             converter.convert(file)
 
