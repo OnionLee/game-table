6290f8836a9487461bd7df1a6dc65b524ad3487c
Ssangyun <sangyun3631@gmail.com>
2016-08-19 22:45:23 +0900

    Add Lost file

--- GameTable-Unity-Sample/Assets/Sample-Unity/TableSystem/TableLocator.cs
+++ GameTable-Unity-Sample/Assets/Sample-Unity/TableSystem/TableLocator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public static class TableLocator 
 {
-	// Property
 	public static CharacterTable CharacterTable { get; private set; }
 	public static SkillTable SkillTable { get; private set; }
 
