aaf81eddf70afbb8550f84cc55b3d914e97db1b0
onionlee <rkdgml6332@gmail.com>
2016-08-26 01:59:36 +0900

    TablePostprocessor 수정

--- GameTable-Unity/Assets/GameTable-TableSystem/Core/Editor/TablePostprocessor.cs
+++ GameTable-Unity/Assets/GameTable-TableSystem/Core/Editor/TablePostprocessor.cs
@@ -7,49 +7,30 @@ using System.Diagnostics;
 
 public class TablePostProcessor : AssetPostprocessor 
 {
-	// Full path to the Asset
-	static string FullPathToExcel = Application.dataPath + "/GameTable-AutomaticConverter/Editor/Excel/";
+	//Edit this field
+	//Excel File Path
+	static string kExcelFilesPath = "/Sample/Excel";
 
-	// Excel file Improt path
-	static string ExcelPath = null;
-
-	// Python script path
-	static string ConverterPath = Application.dataPath + "/GameTable-AutomaticConverter/Editor/Automatic Converter.lnk";
-
-	static bool isInitialize = false;
-
-	private static void Initialize()
-	{
-		string[] splitPath = FullPathToExcel.Split('/');
-
-		// Assets/GameTable-AutomaticConverter/Editor/Excel/ - 4 form behind
-		ExcelPath =	splitPath[splitPath.Length - 5] + "/" +
-					splitPath[splitPath.Length - 4] + "/" +
-					splitPath[splitPath.Length - 3] + "/" +
-					splitPath[splitPath.Length - 2] + "/";
-
-		isInitialize = true;
-	}
+	//Edit this field
+	//Converter Path
+	static string kConverterPath = "/Sample/run.bat";
 
 	private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
 	{
-		if(isInitialize == false)
-			Initialize();
-
-		// Imported Asset
 		foreach (string filePath in importedAssets)
 		{
-			// Check is this temp file
-			if (filePath.StartsWith("~"))
+			if (filePath.Contains (kExcelFilesPath) == false)
 				continue;
 
 			// Check is this Excel file
 			if (filePath.EndsWith(".xlsx") == false)
 				continue;
+			
+			// Check is this temp file
+			if (filePath.StartsWith("~"))
+				continue;
 
-			// Check is this in ExcelPath
-			if (filePath.StartsWith(ExcelPath))
-				Convert();
+			Convert ();
 		}
 	}
 
@@ -67,7 +48,7 @@ public class TablePostProcessor : AssetPostprocessor
 
 	private static void ConvertExcelToJson()
 	{
-		Process.Start(ConverterPath, "").WaitForExit();
+		UnityEngine.Debug.Log (Application.dataPath + kConverterPath);
+		Process.Start(Application.dataPath + kConverterPath, "").WaitForExit();
 	}
 }
-
