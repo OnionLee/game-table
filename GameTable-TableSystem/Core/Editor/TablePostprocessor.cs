7022921dbe3df0350d6925b3cb2f2a8b720f124d
onionlee <rkdgml6332@gmail.com>
2016-12-18 04:34:26 +0900

    잘못된 코드 수정

--- GameTable-TableSystem/Core/Editor/TablePostprocessor.cs
+++ GameTable-TableSystem/Core/Editor/TablePostprocessor.cs
@@ -7,67 +7,46 @@ using System.Diagnostics;
 
 public class TablePostProcessor : AssetPostprocessor 
 {
-	// Full path to the Asset
-	static string FullPathToExcel = Application.dataPath + "/GameTable-AutomaticConverter/Editor/Excel/";
-
-	// Excel file Improt path
-	static string ExcelPath = null;
-
-	// Python script path
-	static string ConverterPath = Application.dataPath + "/GameTable-AutomaticConverter/Editor/Automatic Converter.lnk";
-
-	static bool isInitialize = false;
-
-	private static void Initialize()
-	{
-		string[] splitPath = FullPathToExcel.Split('/');
-
-		// Assets/GameTable-AutomaticConverter/Editor/Excel/ - 4 form behind
-		ExcelPath =	splitPath[splitPath.Length - 5] + "/" +
-					splitPath[splitPath.Length - 4] + "/" +
-					splitPath[splitPath.Length - 3] + "/" +
-					splitPath[splitPath.Length - 2] + "/";
-
-		isInitialize = true;
-	}
+	//Edit this field
+	//Root Path
+	static string kRootPath = "/GameTable-Converter";
+		
+	static string kExcelFilesPath = kRootPath + "/Excel";
+	static string kConverterPath = kRootPath + "/run.bat";
 
 	private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) 
 	{
-		if(isInitialize == false)
-			Initialize();
-
-		// Imported Asset
 		foreach (string filePath in importedAssets)
 		{
-			// Check is this temp file
-			if (filePath.StartsWith("~"))
+			if (filePath.Contains (kExcelFilesPath) == false)
 				continue;
 
 			// Check is this Excel file
 			if (filePath.EndsWith(".xlsx") == false)
 				continue;
+			
+			// Check is this temp file
+			if (filePath.StartsWith("~"))
+				continue;
 
-			// Check is this in ExcelPath
-			if (filePath.StartsWith(ExcelPath))
-				Convert();
+			Convert ();
 		}
 	}
 
 	private static void Convert()
 	{
-		// Logging
-		UnityEngine.Debug.Log("Excecute Automatic Cconvert");
+		UnityEngine.Debug.Log("Excecute Convert");
 
-		// Convert
 		ConvertExcelToJson();
 
-		// Refresh Assets folder
 		AssetDatabase.Refresh();
 	}
 
 	private static void ConvertExcelToJson()
 	{
-		Process.Start(ConverterPath, "").WaitForExit();
+		var info = new ProcessStartInfo ();
+		info.FileName = Application.dataPath + kConverterPath;
+		info.WorkingDirectory = Application.dataPath + kRootPath;
+		Process.Start (info);
 	}
 }
-
